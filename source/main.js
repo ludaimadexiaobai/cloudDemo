d8ac8a90447aa8e5cd11e6135348a68ecf649b9c
wenjun.wang <wenjun.wang@greedyint.com>
2018-04-23 22:47:37 +0800

    代码微调

--- source/main.js
+++ source/main.js
@@ -1,11 +1,11 @@
-import Vue from 'vue';
-import routers from './router.js';
+import Vue from 'vue'
+import routers from './router.js'
 import app from './app.vue'
-import spui from 'spui';
-import VueRouter from 'vue-router';
+import spui from 'spui'
+import VueRouter from 'vue-router'
 
-Vue.use(VueRouter);
-Vue.use(spui);
+Vue.use(VueRouter)
+Vue.use(spui)
 
 const router = new VueRouter({
     mode: 'history', // 切换路径模式，变成history模式,不然路径为/#/home 
