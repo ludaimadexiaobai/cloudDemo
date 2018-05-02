9cc07734b43106daad6413af46a8a952041a95ca
wenjun.wang <wenjun.wang@greedyint.com>
2018-05-02 21:00:47 +0800

    v0.1--新安装依赖项；导航调整；

--- src/components/side-nav.vue
+++ src/components/side-nav.vue
@@ -6,43 +6,78 @@
                 <h3>校宝云服务平台</h3>
             </a>
         </div>
+        <Menu :theme="theme" accordion>
+            <MenuItem name="user">
+                <Icon type="ios-people" size="18"></Icon>
+                    用户管理
+            </MenuItem>
+            <MenuItem name="organization">
+                <Icon type="home" size="18"></Icon>
+                    机构管理
+            </MenuItem>
+            <Submenu name="sms">
+                <template slot="title">
+                    <Icon type="ios-email" size="18"></Icon>
+                    短信管理
+                </template>
+                <MenuItem name="smsSearch">短信查询</MenuItem>
+                <MenuItem name="smsSet">短信配置</MenuItem>
+                <MenuItem name="smsTemplate">短信模板配置</MenuItem>
+                <MenuItem name="smsPackage">机构短信包</MenuItem>
+            </Submenu>
+            <Submenu name="wechats">
+                <template slot="title">
+                    <Icon type="chatbubbles" size="18"></Icon>
+                    微信消息管理
+                </template>
+                <MenuItem name="wechatsSearch">微信消息查询</MenuItem>
+                <MenuItem name="wechatsSet">微信消息配置</MenuItem>
+                <MenuItem name="wechatsThird">微信第三方平台配置</MenuItem>
+            </Submenu>
+            <Submenu name="dingding">
+                <template slot="title">
+                    <Icon type="chatbox-working" size="18"></Icon>
+                    钉钉消息管理
+                </template>
+                <MenuItem name="dingdingSearch">钉钉消息查询</MenuItem>
+                <MenuItem name="dingdingSet">钉钉消息配置</MenuItem>
+            </Submenu>
+            <MenuItem name="docLibrary">
+                <Icon type="ios-paper" size="18"></Icon>
+                    文档库
+            </MenuItem>
+        </Menu>
         
-        <ul class="nav-list">
-            <li class="sideLi"><router-link to="/addemailset"><Icon type="ios-gear" size="18"></Icon><span class="sideSpan">添加短信配置</span></router-link></li>
-            <li class="sideLi"><router-link to="/user"><Icon type="person" size="18"></Icon><span class="sideSpan">用户管理</span></router-link></li>
-            <li class="sideLi"><router-link to="/app"><Icon type="android-apps" size="18"></Icon><span class="sideSpan">应用管理</span></router-link></li>
-            <li class="sideLi"><router-link to="/email"><Icon type="email" size="18"></Icon><span class="sideSpan">短信配置</span></router-link></li>
-            <li class="sideLi"><router-link to="/organization"><Icon type="home" size="18"></Icon><span class="sideSpan">机构管理</span></router-link></li>
-            <li class="sideLi"><router-link to="/emailmodel"><Icon type="android-clipboard" size="18"></Icon><span class="sideSpan">短信模板配置</span></router-link></li>
-            <!-- <li><router-link to="/login">login</router-link></li> -->
-        </ul>
-        <a href="#" class="toggle-nav"><Icon type="chevron-left" size="12" color="#fff" class="sideBtn"></Icon></a>
-        <!--  @click="clickFun" -->
     </div>
 </template>
-<style>
-.sideNav{ position: relative; z-index: 10; top:0; left:0; bottom:0; width:230px; min-height: 100vh; background-color: #373d47; transition: all .2s;}
-.sideNav.open {flex: 0 0 230px; max-width: 230px; min-width: 230px; width: 230px;}
-.sideNav.close {flex: 0 0 52px; max-width: 52px; min-width: 52px; width: 52px;}
-.sideSpan{display:none;}
-.nav-list{padding-top: 20px;}
-.sideLi{height: 40px; line-height: 40px; padding-left: 20px; font-size:14px; color:#fff; cursor: pointer;}
-.sideLi a{color:#aaa;}
-.sideLi.active a{color:#fff;}
-.sideLi i{vertical-align: middle;}
-.sideSpan{display: inline-block; margin-left: 10px; vertical-align: middle;}
-.home{padding:5px 8px; display: block; height: 60px;}
-.home img{ width:35px;margin-top: 7px;}
-.home h3{display: inline-block; margin-left: 20px; line-height: 50px; font-size: 18px; color:#979da7; vertical-align: top; font-weight: normal;}
-.app-logo:hover,.sideLi:hover {background-color: #24272d;}
-.app-logo:hover h3{color: #fff;}
-.toggle-nav{position: fixed; top:60px; right: 0; height: 30px;}
+<style lang="less">
+.sideNav {position: relative; z-index: 10; top: 0; left: 0; bottom: 0; width: 240px; min-height: 100vh; background-color: #495060; transition: all 0.2s;
+  &.open {flex: 0 0 240px; max-width: 240px; min-width: 240px; width: 240px;}
+  &.close {flex: 0 0 52px; max-width: 52px; min-width: 52px; width: 52px;}
+}
+.home {padding: 5px 8px; display: block; height: 60px; text-align: center;
+  img {width: 35px; margin-top: 7px;}
+  h3 {display: inline-block; margin-left: 10px; line-height: 50px; font-size: 18px; color: #979da7; vertical-align: top; font-weight: normal; }
+  &:hover {background-color: #363e4f;}
+  &:hover h3 {color: #fff;}
+}
+.nav-list {padding-top: 20px;}
+.sideLi {height: 40px; line-height: 40px; padding-left: 20px; font-size: 14px; color: #fff; cursor: pointer;
+  a {color: #aaa;}
+  &:hover,&.active {background-color: #24272d;}
+  &.active a {color: #fff;}
+  i {vertical-align: middle;}
+}
+.sideSpan {display: inline-block; margin-left: 10px; vertical-align: middle;}
 </style>
 <script>
 export default {
-  name: 'side-nav',
-  data () {
-      return{}
+  name: "side-nav",
+  data() {
+    return {
+        theme: 'dark'
+    }
   }
-}
+};
 </script>
+
